5bc783e33636085eab10241b00c888eea07f7bcd
sop-101 <sopiaangela1606@gmail.com>
2026-06-13 22:23:56 +0800

    Create email_api_config.php

---
+++ email_api_config.php
@@ -0,0 +1,8 @@
+<?php
+// EmailDetective API Configuration
+// Free tier: 1,000 verifications per month
+// Website: https://emaildetective.io/
+
+define('EMAIL_API_KEY', 'MmE0YTNiMWE5NzZkZjNiNTc5MGU6NWQ1MjgwMWI2ODI2OTY2YTI5Nzc');
+define('EMAIL_API_URL', 'https://api.emaildetective.io/v1/verify');
+?>
