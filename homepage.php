aeeafb7a48a27e8c64fa8e558bc3a75f7bf72429
Makokitsume <markbuquel12345@gmail.com>
2026-06-15 00:12:49 +0800

    Change title to 'Brgy 727 Dengue Campaign System'
    Updated the title and content for the dengue campaign.

--- homepage.php
+++ homepage.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Brgy 727 Disaster & Health Risk Monitoring System</title>
+    <title>Brgy 727 Dengue Campaign System</title>
     <link rel="stylesheet" href="homepage.css">
 </head>
 
